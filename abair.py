92494a931cd6e639e22ef7f5bf7c52091024cde1
Eoghan Quirke <eoghanquirke@gmail.com>
2022-05-08 18:09:35 +0100

    quality of life change

--- abair.py
+++ abair.py
@@ -11,6 +11,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name='abair')
 async def say(ctx, *args):
 
+	if len(args) < 2:
+		return
+	
 	dialect = args[0]
 	if map_dialect(dialect.upper()) == None:
 		await ctx.send("Roghnaigh canúint led thoil. Tá trí rogha agat. GM (Gaolainn na Mumhan, Corca Dhuibhne), GU (Gaeilig Uladh, Gaoth Dobhair) agus GC (Gaeilge Chonnacht, Conamara)")
