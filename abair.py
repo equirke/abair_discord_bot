1dc0108617a69ef78618eb6c3d28d668fbc6030c
Eoghan Quirke <eoghanquirke@gmail.com>
2022-05-08 18:07:16 +0100

    quality of life change

--- abair.py
+++ abair.py
@@ -9,11 +9,20 @@ intents = Intents(messages=True, guilds=True)
 bot = commands.Bot(command_prefix='a!', intents=intents)
 
 @bot.command(name='abair')
-async def say(ctx, dialect, phrase):
-	if map_dialect(dialect) == None:
-		await ctx.send("Roghnaigh canúint led thoil. Tá trí rogha agat. GM (Gaolainn na Mumhan, Corca Dhuibhne), GU (Gaeilig Uladh, Gaoth Dobhar) agus GC (Gaeilge Chonnacht, Conamara)")
+async def say(ctx, *args):
+
+	dialect = args[0]
+	if map_dialect(dialect.upper()) == None:
+		await ctx.send("Roghnaigh canúint led thoil. Tá trí rogha agat. GM (Gaolainn na Mumhan, Corca Dhuibhne), GU (Gaeilig Uladh, Gaoth Dobhair) agus GC (Gaeilge Chonnacht, Conamara)")
+		
 	if len(phrase) > 2000:
 		await ctx.send("uasmhéid 2000 caractar")
+	
+	phrase = ""
+	for word in args[1:]:
+		phrase += word + " "
+	
+	phrase = phrase[:-1]
 	ipa_text, sound = get_pronounciation(phrase, map_dialect(dialect))
 	await ctx.send(phrase + "\n" + ipa_text, file=File(io.BytesIO(sound), filename="abair.mp3"))
 	
